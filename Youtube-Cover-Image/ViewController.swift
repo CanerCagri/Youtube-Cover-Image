48642b5ebe154971e49e21355ae3e9f6985df29f
CanerCagri <canerrcagrii@gmail.com>
2023-07-05 18:09:55 +0300

    Fetching cover image completed.

--- Youtube-Cover-Image/ViewController.swift
+++ Youtube-Cover-Image/ViewController.swift
@@ -6,14 +6,56 @@
 //
 
 import UIKit
+import LinkPresentation
+import MobileCoreServices
 
 class ViewController: UIViewController {
-
+    
+    private let imageView: UIImageView = {
+        var image = UIImageView()
+        image.backgroundColor = .systemBackground
+        image.layer.cornerRadius = 15
+        image.clipsToBounds = true
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupLayout()
+        
+        let url = URL(string: "https://www.youtube.com/watch?v=rWIPn3x31aM&t=1036s&ab_channel=Hozho")
+        let metadataProvider: LPMetadataProvider = .init()
+        
+        metadataProvider.startFetchingMetadata(for: url!) { metadata, error in
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            metadata?.imageProvider?.loadFileRepresentation(forTypeIdentifier: kUTTypeImage as String) { [weak self] url, imageProviderError in
+                if let imageProviderError = imageProviderError {
+                    print(imageProviderError)
+                    return
+                }
+                
+                guard let urlPath = url?.path, let image: UIImage = .init(contentsOfFile: urlPath) else { return }
+                
+                DispatchQueue.main.async {
+                    self?.imageView.image = image
+                }
+               
+            }
+        }
     }
-
-
+    
+    private func setupLayout() {
+        view.backgroundColor = .systemBackground
+        view.addSubview(imageView)
+        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 300).isActive = true
+    }
 }
-
